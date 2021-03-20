8cf835c20dbe537224eb3698a2b4a14ba67c0b4c
qiakai <2368431928@qq.com>
2021-03-21 00:06:49 +0800

    第十九次提交

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Main from '../views/Main.vue'
+import Home from '../views/Home.vue'
 import Article from '../views/Article.vue'
+import Hero from '../views/Hero.vue'
 
 Vue.use(VueRouter)
 
@@ -12,9 +13,10 @@ const routes = [
     component: Main,
     children: [
       {path: '/',name: 'home',component: Home},
-      {path: '/articles/:id',name: 'articles',component: Article, props: true},
+      {path: '/articles/:id',name: 'article',component: Article, props: true},
     ]
   },
+  {path: '/heroes/:id',name: 'hero',component: Hero, props: true},
   {
     path: '/about',
     name: 'About',
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -63,12 +63,21 @@
     <m-list-card icon="card-hero" title="英雄列表" :categories="heroCats">
       <template #items="{ category }">
         <div class="d-flex flex-wrap" style="margin: 0 -0.4rem">
-          <div class="p-2 text-center"
-          style="width: 20%;"
-           v-for="(hero, i) in category.heroList" :key="i">
-            <img :src="hero.avatar" class="w-100" />
+          <router-link
+            tag="div"
+            :to="`/heroes/${hero._id}`"
+            class="p-2 text-center"
+            style="width: 20%"
+            v-for="(hero, i) in category.heroList"
+            :key="i"
+          >
+            <img
+              :src="hero.avatar"
+              class="w-100"
+              style="border: 1px solid #258df2; border-radius: 10px 0 10px 0"
+            />
             <div>{{ hero.name }}</div>
-          </div>
+          </router-link>
         </div>
       </template>
     </m-list-card>
@@ -141,9 +150,5 @@ export default {
       border-left: none;
     }
   }
-}
-img{
-    border: 1px solid map-get($map: $colors, $key: "blue");
-    border-radius: 10px 0 10px 0;
 }
 </style>
