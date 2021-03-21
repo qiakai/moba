f789e9f4bd0fd7bb40012e64b3aca7421ef48fae
qiakai <2368431928@qq.com>
2021-03-21 21:30:37 +0800

    第二十次提交

--- web/src/views/Hero.vue
+++ web/src/views/Hero.vue
@@ -10,23 +10,110 @@
     </div>
     <div class="top" :style="{ 'background-image': `url(${model.banner})` }">
       <div class="info text-white p-3 h-100 d-flex flex-column jc-end">
-        <div>{{ model.title }}</div>
+        <div class="fs-sm">{{ model.title }}</div>
         <h2 class="my-2">{{ model.name }}</h2>
-        <div>{{ model.categories.map((v) => v.name).join("/") }}</div>
+        <div class="fs-sm">
+          {{ model.categories.map((v) => v.name).join("/") }}
+        </div>
         <div class="d-flex jc-between">
           <div class="scores" v-if="model.scores">
             <span>难度</span>
             <span class="badge bg-primary">{{ model.scores.difficult }}</span>
-            <span>难度</span>
+            <span>技巧</span>
             <span class="badge bg-blue">{{ model.scores.skills }}</span>
-            <span>难度</span>
+            <span>攻击</span>
             <span class="badge bg-danger">{{ model.scores.attack }}</span>
-            <span>难度</span>
+            <span>生存</span>
             <span class="badge bg-dark">{{ model.scores.survive }}</span>
           </div>
-          <router-link to="/" tag="span" class="text-grey fs-sm">皮肤：2 &gt;</router-link>
+          <router-link to="/" tag="span" class="text-grey fs-sm"
+            >皮肤：2 &gt;</router-link
+          >
         </div>
       </div>
+    </div>
+    <!-- end of top -->
+    <div>
+      <swiper>
+        <swiper-slide>
+          <div>
+            <div class="p-3 bg-white border-bottom">
+              <div class="d-flex">
+                <router-link tag="button" to="/" class="btn btn-lg flex-1">
+                  <i class="iconfont icon-menu1"></i>
+                  英雄介绍视频</router-link
+                >
+                <router-link tag="button" to="/" class="btn btn-lg flex-1">
+                  <i class="iconfont icon-menu1"></i>
+                  一图识英雄</router-link
+                >
+              </div>
+              <!-- skills -->
+              <div class="skills mt-4">
+                <div class="d-flex jc-around">
+                  <img
+                    class="icon"
+                    @click="currentSkillIndex = i"
+                    :class="{ active: currentSkillIndex === i }"
+                    :src="item.icon"
+                    v-for="(item, i) in model.skills"
+                    :key="item.name"
+                  />
+                </div>
+                <div v-if="currentSkill">
+                  <div class="d-flex pt-4 pb-3">
+                    <h3 class="m-0">{{ currentSkill.name }}</h3>
+                    <span class="text-grey-1 ml-4">
+                      （冷却值：{{ currentSkill.delay }} 消耗：{{
+                        currentSkill.cost
+                      }}）
+                    </span>
+                  </div>
+                  <p>{{ currentSkill.description }}</p>
+                  <div class="border-bottom"></div>
+                  <p class="text-grey-1">小提示：{{ currentSkill.tips }}</p>
+                </div>
+              </div>
+            </div>
+
+            <m-card plain icon="menu1" title="出装推荐" class="hero-items">
+              <div class="fs-xl">顺风出装</div>
+              <div class="d-flex jc-around text-center mt-3">
+                <div v-for="item in model.items1" :key="item.name">
+                  <img :src="item.icon" class="icon">
+                  <div class="fs-xs">{{item.name}}</div>
+                </div>
+              </div>
+              <div class="border-bottom mt-3"></div>
+              <div class="fs-xl mt-3">逆风出装</div>
+              <div class="d-flex jc-around text-center mt-3">
+                <div v-for="item in model.items2" :key="item.name">
+                  <img :src="item.icon" class="icon">
+                  <div class="fs-xs">{{item.name}}</div>
+                </div>
+              </div>
+            </m-card>
+
+            <m-card plain icon="menu1" title="使用技巧">
+              <p class="m-0">{{model.usageTips}}</p>
+            </m-card>
+            <m-card plain icon="menu1" title="对抗技巧">
+              <p class="m-0">{{model.battleTips}}</p>
+            </m-card>
+            <m-card plain icon="menu1" title="团战技巧">
+              <p class="m-0">{{model.teamTips}}</p>
+            </m-card>
+            <m-card plain icon="menu1" title="英雄关系">
+              <div class="fs-xl">最佳搭档</div>
+              <div class="d-flex pt-3" v-for="item in model.partners" :key="item.name">
+                <img :src="item.hero.avatar" alt="" height="50" style="border: 1px solid #258df2; border-radius: 0 10px 0 10px">
+                <p class="flex-1 m-0 ml-3">{{item.description}}</p>
+              </div>
+              <div class="border-bottom mt-3"></div>
+            </m-card>
+          </div>
+        </swiper-slide>
+      </swiper>
     </div>
   </div>
 </template>
@@ -39,7 +126,13 @@ export default {
   data() {
     return {
       model: null,
+      currentSkillIndex: 0,
     };
+  },
+  computed: {
+    currentSkill() {
+      return this.model.skills[this.currentSkillIndex];
+    },
   },
   methods: {
     async fetch() {
@@ -54,11 +147,46 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../assets/scss/_variables.scss";
 .page-hero {
   .top {
     height: 50vw;
     background: #fff no-repeat top center;
     background-size: 100% 100%;
+  }
+  .info {
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
+    .scores {
+      .badge {
+        margin: 0 0.25rem;
+        display: inline-block;
+        width: 1rem;
+        height: 1rem;
+        line-height: 0.9rem;
+        text-align: center;
+        border-radius: 50%;
+        font-size: 0.6rem;
+        border: 1px solid rgba(255, 255, 255, 0.2);
+      }
+    }
+  }
+  .skills {
+    img.icon {
+      width: 70px;
+      height: 70px;
+      border: 3px solid map-get($map: $colors, $key: "white");
+      &.active {
+        border-color: map-get($map: $colors, $key: "primary");
+      }
+      border-radius: 45%;
+    }
+  }
+  .hero-items {
+    img.icon{
+      width: 45px;
+      height: 45px;
+      border-radius: 50%;
+    }
   }
 }
 </style>
